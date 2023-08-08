22c658f44c4f8002930fb3999fbabe422841fe00
Z-Jay <zjaytumamaop@gmail.com>
2023-08-08 13:37:52 -0400

    added code to make dragging your mouse over the grid it changes color to green and fades in 500 ms

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 //javascript 
 
-function hover() {
-    div.style.cssText = "background-color: green";
-}
-
-function fade() {
-    document.getElementById('div').style.animation = 'fade 3s 2 '
-}
-   
     for(i = 0;i < 256; i++){
         const container = document.querySelector('.container');
         const newDiv = document.createElement('div');
-
+        newDiv.classList.add('box')
         container.appendChild(newDiv);
     }
+function hover() {
+    this.classList.add('hoveringDiv')
+}
 
-    document.querySelector('#div').addEventListener('mouseover', hover())
-    document.querySelector('#div').addEventListener('mouseout', fade())
+function fade() {
+    const element = this;
+    setTimeout(function () {
+        element.classList.remove('hoveringDiv');
+    }, 500);
+    
+}
+
+   
+const divs = document.querySelectorAll('.box')
+divs.forEach(div => {
+    div.addEventListener('mouseover', hover);
+    div.addEventListener('mouseout', fade);
+  });
+
+    
