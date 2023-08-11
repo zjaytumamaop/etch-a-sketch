4112ff260a1b6cd9b0ceb4717fc51a1cb2cdbbd1
Z-Jay <zjaytumamaop@gmail.com>
2023-08-11 08:27:56 -0400

    working on holding and clickable evnet actions

--- script.js
+++ script.js
@@ -16,35 +16,57 @@ function fade() {
     }, 500);
     
 }
+
+function click(){
+     this.classList.add('clickingDiv');
+}
 function setGrid() {
     let gridNumber = document.getElementById("slider").value;
     //telling css how many boxes per row  and column
     document.getElementById('grid').style.gridTemplateColumns= `repeat(${gridNumber}, 1fr)`;
     document.getElementById('grid').style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
   
+    // while loop to reset the grid
     const container = document.getElementById('grid')
-    while(container.firstChild){// while loop to reset the grid
+    while(container.firstChild){
         container.removeChild(container.firstChild); 
     }
 
-
-    for(i = 0;i < (gridNumber * gridNumber); i++){ // for loop to add divs into the grid
-        const container = document.querySelector('.container');
+    // for loop to add divs into the grid
+    for(i = 0;i < (gridNumber * gridNumber); i++){
         const newDiv = document.createElement('div');
         newDiv.classList.add('box')
         container.appendChild(newDiv);
+    
     }
+        //addes eventlisteners to the divs 
+    const divs = document.querySelectorAll('.box');
+    divs.forEach((div) => {
+    div.addEventListener('mouseover', hover);
+    div.addEventListener('mouseout', fade);
+    div.addEventListener('mousedown', startHold);
+    div.addEventListener('mouseup', cancelHold);
+
+    
+        });         
+    
+}
+const holdDuration = 1000;
+
+function startHold() {
+    holdTimer = setTimeout(function(){
+        this.classList.add('clickingDiv');
+    }.bind(this), holdDuration);
 }
 
+function cancelHold() {
+    clearTimeout(holdTimer);
+}
 
     const grid = document.getElementById('updateGrid');
     grid.addEventListener('click', setGrid);
 
 
-    const divs = document.querySelectorAll('.box')
-    divs.forEach(div => {
-        div.addEventListener('mouseover', hover);
-        div.addEventListener('mouseout', fade);
-    });
+    
 
     
