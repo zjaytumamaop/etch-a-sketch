c5d25d5b07a81a7ca56c3a9f0fd73aa06888e44b
Z-Jay <zjaytumamaop@gmail.com>
2023-08-14 01:54:53 -0400

    cleaned up code, organized code into functions and updating into one function 'update'

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //javascript 
  let buttonNum = 0;
+ let gridNumber = 16;
 
 
 
@@ -10,45 +11,53 @@ function click() {
 }
 
 function unclick(){
-     this.classList.remove('hoveringDiv');
+    this.classList.remove('hoveringDiv');
 }
 
 function updateGridNum(){
     const Num = document.getElementById('buttonNum');
     Num.innerHTML = buttonNum + ' X ' + buttonNum;
 }
-
-function setGrid() {
-    let gridNumber = document.getElementById("slider").value;
-    buttonNum = gridNumber;
-    //telling css how many boxes per row  and column
+function settingGrid(){//telling css how many boxes per row  and column
     document.getElementById('grid').style.gridTemplateColumns= `repeat(${gridNumber}, 1fr)`;
     document.getElementById('grid').style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
-  
-    // while loop to reset the grid
-    const container = document.getElementById('grid')
-    while(container.firstChild){
-        container.removeChild(container.firstChild); 
-    }
+}
 
-    // for loop to add divs into the grid
-   
-    
+function resetGrid() {// while loop to reset the grid
+    const container = document.getElementById('grid');
+        while(container.firstChild){
+            container.removeChild(container.firstChild); 
+        }
+}
+
+function addDivs(){ // for loop to add divs into the grid
+    const container = document.getElementById('grid')
     for(i = 0;i < (gridNumber * gridNumber); i++){
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('box')
-        container.appendChild(newDiv);
-    
-    }
-        //addes eventlisteners to the divs 
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('box');
+            container.appendChild(newDiv);
+        }
+}
+
+function addEventListener(){//addes eventlisteners to the divs 
     const divs = document.querySelectorAll('.box');
     divs.forEach((div) => {
     div.addEventListener('mouseover', click);
-    div.addEventListener('mousedown', unclick )
+    div.addEventListener('mousedown', unclick );
+    })
+}
 
+function setGridValue(){
+    gridNumber = document.getElementById("slider").value;
+    buttonNum = gridNumber;  
+}
+function setGrid() {
+    setGridValue();
+    settingGrid();
+    resetGrid();
+    addDivs();
     updateGridNum();
-        });         
-    
+    addEventListener();
 }
 
     const colorPicker = document.getElementById('colorPicker');
